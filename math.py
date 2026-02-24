547c4d0b22c35556fa169cefc45d74e389a439c4
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-25 03:29:43 +0600

    just add some

--- math.py
+++ math.py
@@ -1,14 +1,23 @@
-# print("Welcome to the Math Module!")
+# # print("Welcome to the Math Module!")
+# import math
+# valobasa =100
+# prem =50000000
+# print(math.sqrt(valobasa))
+# print(math.sqrt(prem))
+# #round add
+# prem = 50000000
+# love = 100
+# total = (math.sqrt(prem + love))
+# print ("valobasar more zak:",round(total,2))
+#traingle tropic
 import math
-valobasa =100
-prem =50000000
-print(math.sqrt(valobasa))
-print(math.sqrt(prem))
-#round add
-prem = 50000000
-love = 100
-total = (math.sqrt(prem + love))
-print ("valobasar more zak:",round(total,2))
+def triangle_area(base, height):
+    return 0.5 * base * height
+base = 10
+height = 5
+area = triangle_area(base, height)
+print("The area of the triangle is:", area)
+
 
 
 
