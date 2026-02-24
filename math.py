16983bee45c88ebeaba7dfb9b13b13d51a8366dd
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-25 03:26:33 +0600

    just add some basixcc

--- math.py
+++ math.py
@@ -1,1 +1,16 @@
-print("Welcome to the Math Module!")
+# print("Welcome to the Math Module!")
+import math
+valobasa =100
+prem =50000000
+print(math.sqrt(valobasa))
+print(math.sqrt(prem))
+#round add
+prem = 50000000
+love = 100
+total = (math.sqrt(prem + love))
+print ("valobasar more zak:",round(total,2))
+
+
+
+
+
