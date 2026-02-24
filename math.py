20c2397deec3379935ce7525937f31085201528f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-25 03:44:58 +0600

    maybe math function end now

--- math.py
+++ math.py
@@ -1,29 +1,44 @@
-# # # print("Welcome to the Math Module!")
+# # # # print("Welcome to the Math Module!")
+# # # import math
+# # # valobasa =100
+# # # prem =50000000
+# # # print(math.sqrt(valobasa))
+# # # print(math.sqrt(prem))
+# # # #round add
+# # # prem = 50000000
+# # # love = 100
+# # # total = (math.sqrt(prem + love))
+# # # print ("valobasar more zak:",round(total,2))
+# # #traingle tropic
 # # import math
-# # valobasa =100
-# # prem =50000000
-# # print(math.sqrt(valobasa))
-# # print(math.sqrt(prem))
-# # #round add
-# # prem = 50000000
-# # love = 100
-# # total = (math.sqrt(prem + love))
-# # print ("valobasar more zak:",round(total,2))
-# #traingle tropic
+# # def triangle_area(base, height):
+# #     return 0.5 * base * height
+# # base = 10
+# # height = 5
+# # area = triangle_area(base, height)
+# # print("The area of the triangle is:", area)
+# # #circle area
 # import math
-# def triangle_area(base, height):
-#     return 0.5 * base * height
-# base = 10
-# height = 5
-# area = triangle_area(base, height)
-# print("The area of the triangle is:", area)
-# #circle area
+# def circle_area(radius):
+#     return math.pi * radius ** 2
+# radius = 7
+# area = circle_area(radius)
+# print("The area of the circle is:", area)
+#traingle more
 import math
-def circle_area(radius):
-    return math.pi * radius ** 2
-radius = 7
-area = circle_area(radius)
-print("The area of the circle is:", area)
+base = 10
+height = 5
+area = 0.5 * base * height
+print("The area of the triangle is:", area)
+angle_radians = math.atan(height / base)
+angle_degrees = math.degrees(angle_radians)
+print("The angle of the triangle is:", round(angle_degrees), "degrees")
+# upper value
+value = 3.7
+upper_value = math.ceil(value)
+print("The upper value of", value, "is:", upper_value)
+down_value = math.floor(value)
+print("The down value of", value, "is:", down_value)
 
 
 
