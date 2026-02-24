610397e6da8c1196108b63808e838d0c609e69d9
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-02-25 05:19:43 +0600

    i dont know

--- plan.py
+++ plan.py
@@ -1,1 +1,15 @@
-print("i am a plan boy")
+# # print("i am a plan boy")
+# import math
+# planet = "earth"
+# love = "nazmul"
+# activity = "coding"
+# tweet = "i love coding" + love + "and i am from" + planet "nazmul how are you" + activity "i dont know how to do this"+""
+# S
+# print(tweet)
+# #play with string concatenation and print the result
+import math
+radius = 5
+area = math.pi * radius ** 2
+print("The area of the circle with radius", radius, "is", area)
+
+
